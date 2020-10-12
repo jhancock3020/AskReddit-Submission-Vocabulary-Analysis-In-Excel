62e33ad2c021f9fd7e316ab24791808282331745
jhancock3020 <69486672+jhancock3020@users.noreply.github.com>
2020-10-12 11:22:48 -0400

    Added Word Count Ranking
    Added word count ranking analysis for each column and added feature that updates information about already viewed questions

--- Reddit_Post_Vocab_Analysis.py
+++ Reddit_Post_Vocab_Analysis.py
@@ -12,7 +12,7 @@ reddit = praw.Reddit(client_id='14 Char Info',
                      user_agent='Project Name')
 #The subreddit object that will be analyzed
 askReddit = reddit.subreddit('askreddit')
-subLimit = 10
+subLimit = 5
 #Gets the top posts from the subreddit
 askRedditTopPosts = askReddit.hot(limit=subLimit)
 #Gets the position (in the context of sheet placement) of newly collected posts
@@ -28,6 +28,7 @@ wb = copy(rb)
 writeSheet = wb.get_sheet(0)
 writeSheet1 = wb.get_sheet(1)
 writeSheet2 = wb.get_sheet(2)
+writeSheet3 = wb.get_sheet(3)
 #FONT SECTION
 #different font styles used
 styleBold = xlwt.easyxf('font: bold 1, color black;')
@@ -51,6 +52,7 @@ submissionList = []
 #if workbook is empty, add these as first row titles of columns
 if(readSheet.nrows <= 0):
     writeSheet.write(0,0, "WORD LIST",styleBold)
+    writeSheet.write(0,1, "Total Num",styleBold)
     writeSheet1.write(0,0,"Post ID",styleBold)
     writeSheet1.write(0,1,"Post Title",styleBold)
     writeSheet1.write(0,2,"Author",styleBold)
@@ -62,7 +64,7 @@ if(readSheet.nrows <= 0):
     writeSheet1.write(0,8,"# Of Plat Awards",styleBold)
     writeSheet1.write(0,9,"# Of Gold Awards",styleBold)
     writeSheet1.write(0,10,"# Of Silver Awards",styleBold)
-    
+    writeSheet1.write(0,11,"Was Updated",styleBold)   
 #Save and reopen the workbook and update the readSheets
 wb.save('fileExcel.xls') 
 rb = xlrd.open_workbook('fileExcel.xls')
@@ -99,11 +101,17 @@ for j,submission in enumerate(askRedditTopPosts):
         #READ SUBMISSION TITLE AND COUNT INDIVIDUAL WORDS IN WORDLIST
         #Create a string variable of sub's title and remove unwanted chars 
         editedTitle = submission.title
+        editedTitle = editedTitle.lower()
         for character in removeChar:
             editedTitle = editedTitle.replace(character,"")
-        editedTitle = editedTitle.replace("[Serious]","")        
+        editedTitle = editedTitle.replace("{serious}","")
+        editedTitle = editedTitle.replace("[serious]","")
+        editedTitle = editedTitle.replace("(serious)","")
+        editedTitle = editedTitle.replace("{nsfw}","")
+        editedTitle = editedTitle.replace("[nsfw]","")
+        editedTitle = editedTitle.replace("(nsfw)","")  
         #Then make all letters in the string lowercase before spliting it into a list of strings split by spaces
-        currTitle = editedTitle.lower().split()
+        currTitle = editedTitle.split()
         #go through char list of title words, if the word has already been used, add 1 to the corresponding wordCount index
         for word in currTitle:
             for i,wordInList in enumerate(wordList):
@@ -151,10 +159,52 @@ for j,submission in enumerate(askRedditTopPosts):
         
         #iterates redditRank by +1
         redditRank += 1
+    #If ID is in list
+    elif(submission.id in submissionList):
+        #Go through the rows
+        updateInfo = ""
+        for i in range(readSheet1.nrows):
+            #If ID matches on in list, check to see if any of the numeric values changed and update them on sheet
+            if(submission.id == readSheet1.cell_value(i,0)):
+                if(submission.num_comments != readSheet1.cell_value(i,3)):
+                    updateInfo += "COMMENTS from " + str(int(readSheet1.cell_value(i,3)))+" to " + str(submission.num_comments) + " "
+                    writeSheet1.write(i,3,int(submission.num_comments))
+                if(submission.score != readSheet1.cell_value(i,4)):
+                    updateInfo += "UPVOTES from " + str(int(readSheet1.cell_value(i,4)))+" to " + str(submission.score) + " "
+                    writeSheet1.write(i,4,submission.score)
+                if(submission.upvote_ratio != readSheet1.cell_value(i,5)):
+                    updateInfo += "RATIO from " + str(readSheet1.cell_value(i,5))+" to " + str(submission.upvote_ratio) + " "
+                    writeSheet1.write(i,5,submission.upvote_ratio)
+                #Goes through different possible awards and checks to see if current submission has any, marks them if they do
+                #If there are no awards, set award number to zero
+                numPlat = submission.gildings.get("gid_3")
+                numGold = submission.gildings.get("gid_2")
+                numSilver = submission.gildings.get("gid_2")
+                if numSilver is None:
+                    numSilver = 0
+                numGold = submission.gildings.get("gid_2")
+                if numGold is None:
+                    numGold = 0
+                numPlat = submission.gildings.get("gid_3")
+                if numPlat is None:
+                    numPlat = 0
+                if(numPlat > readSheet1.cell_value(i,8)):
+                    updateInfo += "PLAT from " + str(readSheet1.cell_value(i,8))+" to " + str(submission.gildings.get("gid_3")) + " "
+                    writeSheet1.write(i,8,submission.gildings.get("gid_3"))
+                if(numGold > readSheet1.cell_value(i,9)):
+                    updateInfo += "GOLD from " + str(readSheet1.cell_value(i,9))+" to " + str(submission.gildings.get("gid_2")) + " "
+                    writeSheet1.write(i,9,submission.gildings.get("gid_2"))
+                if(numSilver > readSheet1.cell_value(i,10)):
+                    updateInfo += "SILV from " + str(readSheet1.cell_value(i,10))+" to " + str(submission.gildings.get("gid_1")) + " "
+                    writeSheet1.write(i,10,submission.gildings.get("gid_1"))
+                #If content was updated, write info about it to the las tcell in the row
+                if(updateInfo != ""):
+                    updateInfo += "was updated on " + currDate
+                    writeSheet1.write(i,11,updateInfo)
 
 #WRITE WORD LIST AND CURR WORD COUNT TO SHEET
 #Writes the current date and time of when submissions were received on wordSheet's first row above the current word count list        
-writeSheet.write(0, startCol, currDate+" # of instances of word on this date and time",styleBold)
+writeSheet.write(0, startCol, currDate,styleBold)
 #Rewrites the word list into cells and writes current day's word count to the next blank row
 for i,word in enumerate(wordList):
     writeSheet.write(i+1, 0, word)
@@ -165,19 +215,43 @@ rb = xlrd.open_workbook('fileExcel.xls',formatting_info=True)
 readSheet = rb.sheet_by_index(0)
 readSheet1 = rb.sheet_by_index(1)
 readSheet2 = rb.sheet_by_index(2)
+
 #If an empty cell exists in the word count sheet, make it a zero
 for i in range(readSheet.nrows):
     for j in range(readSheet.ncols):
         if(readSheet.cell_value(i,j) == ''):
             writeSheet.write(i,j,0)
+#Save and reopen the workbook and update the readSheets
+wb.save('fileExcel.xls') 
+rb = xlrd.open_workbook('fileExcel.xls',formatting_info=True)
+readSheet = rb.sheet_by_index(0)
+readSheet1 = rb.sheet_by_index(1)
+readSheet2 = rb.sheet_by_index(2)
+readSheet3 = rb.sheet_by_index(3)
 
+#Goes through each row and add the total number of each word's wordCount instance
+for i in range(readSheet.nrows):
+    #total count of all instances of a word
+    tot = 0
+    for j in range(readSheet.ncols):
+        if(i > 0 and j > 1):
+            tot += readSheet.cell_value(i,j)
+    if(i != 0):
+        writeSheet.write(i,1, tot,styleBold)
+#Save and reopen the workbook and update the readSheets
+wb.save('fileExcel.xls') 
+rb = xlrd.open_workbook('fileExcel.xls',formatting_info=True)
+readSheet = rb.sheet_by_index(0)
+readSheet1 = rb.sheet_by_index(1)
+readSheet2 = rb.sheet_by_index(2)
+readSheet3 = rb.sheet_by_index(3)
 #EDITS TEXT COLOR AND FORMAT OF HIGHEST AND LOWEST NUMBERS OF SUBMISSION INFO
 #Go through the different columns to find the highest and lowest numbers in each
 for i in range(readSheet1.ncols):
     low = 1000000
     high = 0
     for j in range(readSheet1.nrows):
-        if(j != 0 and i > 2 and i != 6 and i != 7 and readSheet1.cell_value(j,i) != ''):
+        if(j != 0 and i > 2 and i != 6 and i != 7 and i != 11 and readSheet1.cell_value(j,i) != ''):
             if(readSheet1.cell_value(j,i) < low):
                 low = readSheet1.cell_value(j,i)
             if(readSheet1.cell_value(j,i) > high):
@@ -186,7 +260,7 @@ for i in range(readSheet1.ncols):
     #If number is lowest (or bool is False), bold and make blue
     #Else, just make it regular font.   
     for j in range(readSheet1.nrows):
-        if(j != 0 and i > 2 and i != 6 and i != 7 and readSheet1.cell_value(j,i) != ''):
+        if(j != 0 and i > 2 and i != 6 and i != 7 and i != 11 and readSheet1.cell_value(j,i) != ''):
             if(readSheet1.cell_value(j,i) == low):
                 writeSheet1.write(j,i,low,styleLow)
             elif(readSheet1.cell_value(j,i) == high):
@@ -196,19 +270,19 @@ for i in range(readSheet1.ncols):
         elif (j != 0 and  (i == 6 or i == 7) and readSheet1.cell_value(j,i) != ''):
             if(readSheet1.cell_value(j,i) == 0):
                 writeSheet1.write(j,i,bool(readSheet1.cell_value(j,i)),styleLow)
-            elif(readSheet1.cell_value(i,6) == 1):
+            elif(readSheet1.cell_value(j,i) == 1):
                 writeSheet1.write(j,i,bool(readSheet1.cell_value(j,i)),styleHigh)
 data = []
 cols2 = 0
-#Fill in 2d list. Each element contains all info on post
+#Fill in 2d list. Each element contains all info on submission
 for j in range(readSheet1.nrows):
     elem = []
-    if(j != 0 and readSheet1.cell_value(j,2) != '' ):
-        elem =[readSheet1.cell_value(j,i)for i in range(readSheet1.ncols)]
+    if(j != 0 and readSheet1.cell_value(j,2) != ''):
+        elem = [readSheet1.cell_value(j,i)for i in range(readSheet1.ncols)]
         data.append(elem)
 #Goes though each column from sheet1 and sorts questions from highest to lowest
 for i in range(11):
-    if(i > 2 and i != 6 and i != 7):
+    if(i > 2 and i != 6 and i != 7 and i != 11):
         data = sorted(data,key=lambda l:l[i],reverse=True)
         writeSheet2.write(0,cols2,readSheet1.cell_value(0,1),styleBold)
         writeSheet2.write(0,cols2+1,readSheet1.cell_value(0,i),styleBold)
@@ -216,5 +290,21 @@ for i in range(11):
             writeSheet2.write(1+j,cols2,data[j][1])
             writeSheet2.write(1+j,cols2+1,data[j][i])
         cols2 += 3
-    
+data = []
+cols1 = 0
+#Fill in 2d list. Each element contains all info on submission
+for j in range(readSheet.nrows):
+    if(j != 0):
+        elem = [readSheet.cell_value(j,i)for i in range(readSheet.ncols)]
+        data.append(elem)
+for j in range(readSheet.ncols):
+    if(j > 0):
+        data = sorted(data,key=lambda x:x[j],reverse=True)
+        num = int(cols1/3)+1
+        for i in range(len(data)):
+            writeSheet3.write(0,cols1,readSheet.cell_value(0,num),styleBold)
+            writeSheet3.write(1+i,cols1,data[i][0])
+            writeSheet3.write(1+i,cols1+1,data[i][j])
+        cols1 += 3
+
 wb.save('fileExcel.xls') 
